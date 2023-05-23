120004b514debb2599e41711cb35aedfd0001b61
JoaoMendes123 <a44869@alunos.isel.pt>
2023-05-23 16:21:44 +0100

    Start of project, created template for cloudFunction to fetch VMs in instance group

---
+++ CFunctionListVMs/src/main/java/ListVMsInGroup.java
@@ -0,0 +1,10 @@
+import com.google.cloud.functions.HttpFunction;
+import com.google.cloud.functions.HttpRequest;
+import com.google.cloud.functions.HttpResponse;
+
+public class ListVMsInGroup implements HttpFunction {
+    @Override
+    public void service(HttpRequest httpRequest, HttpResponse httpResponse) throws Exception {
+
+    }
+}
